ac25b71445a340106d8a637cb65e9611f220a0a4
RaphaelQSmith <49982690+RaphaelQSmith@users.noreply.github.com>
2021-07-27 01:17:47 -0300

    Get and GetAll controller methods

--- Controllers/PizzaController.cs
+++ Controllers/PizzaController.cs
@@ -15,8 +15,20 @@ namespace ContosoPizza.Controllers
         }
 
         // GET all action
+        [HttpGet]
+        public ActionResult<List<Pizza>> GetAll() =>
+        PizzaService.GetAll();
 
         // GET by Id action
+        [HttpGet("{id}")]
+        public ActionResult<Pizza> Get(int id)
+        {
+            var pizza = PizzaService.Get(id);
+            if(pizza == null){
+                return NotFound();
+            }
+            return pizza;
+        }
 
         // POST action
 
